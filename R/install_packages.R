09203377d74c723360665375e06bb2256b433d08
Lars van der Laan <vanderlaanlars@yahoo.com>
2024-01-24 09:32:16 -0800

    update

--- R/install_packages.R
+++
@@ -1,4 +0,0 @@
-.libPaths( c( "~/Rlibs2") )
-devtools::install_github("Larsvanderlaan/conformal")
-devtools::install_github(repo="ryantibs/conformal", subdir="conformalInference")
-devtools::install_cran("reticulate")
