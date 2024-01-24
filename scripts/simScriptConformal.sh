03f7287c22a83467ef8612d9e5e2c52a0530e132
Lars van der Laan <vanderlaanlars@yahoo.com>
2024-01-24 14:19:09 -0800

    hey

--- scripts/simScriptConformal.sh
+++ scripts/simScriptConformal.sh
@@ -2,13 +2,13 @@
 nsims=2500
 export R_LIBS=~/Rlibs2
 export R_LIBS_USER=~/Rlibs2
-for d in 1 2 5 10
+for d in 1 #2 5 10
 do
   for lrnr_name in "gam" "xg" "rf"
     do
-    for shape in 1 2 3 4
+    for shape in 1 #2 3 4
       do
-      for cond_var_type in 1 2
+      for cond_var_type in 1 #2
         do
         sbatch  --export=d=$d,lrnr_name=$lrnr_name,shape=$shape,cond_var_type=$cond_var_type ~/conformal/scripts/simScriptConformal.sbatch
         done
