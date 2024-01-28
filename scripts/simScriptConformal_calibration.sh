d1fd7332ab047a58423685451b01390a80c8c312
Lars van der Laan <vanderlaanlars@yahoo.com>
2024-01-28 10:46:58 -0800

    bug

--- scripts/simScriptConformal_calibration.sh
+++ scripts/simScriptConformal_calibration.sh
@@ -2,13 +2,13 @@
 nsims=2500
 export R_LIBS=~/Rlibs2
 export R_LIBS_USER=~/Rlibs2
-for d in 1 2 3 5 20
+for d in 3 5 20
 do
   for lrnr_name in "xg" "rf"
     do
     for shape in 1 1.5 2 2.5 3
       do
-      for b in 0.3 0.5 0.6
+      for b in 0.6
         do
         sbatch  --export=d=$d,lrnr_name=$lrnr_name,shape=$shape,b=$b,name="calibration" ~/conformal/scripts/simScriptConformal.sbatch
         done
