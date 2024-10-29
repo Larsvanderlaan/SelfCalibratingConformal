b5634215185afb7f6f7d3cb568b40028aed7d07d
Lars van der Laan <lvanderlaan@Larss-MacBook-Pro.local>
2024-10-29 12:21:41 -0700

    final

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SelfCalibratingConformal',
-    version='0.66',
+    version='1.0',
     packages=find_packages(),
     description='A Python implementation of Self-Calibrating Conformal Prediction',
     long_description='A Python implementation of Self-Calibrating Conformal Prediction',
